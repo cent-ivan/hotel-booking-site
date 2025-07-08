1441810d6e58d55536e7c0e2d01a9ef6194b5a84
Vincent_Ivan <vpalomata25@gmail.com>
2025-07-08 18:21:05 +0800

    complete booking summary feature

--- app/blueprints/reservation/rooms_views.py
+++ app/blueprints/reservation/rooms_views.py
@@ -1,15 +1,19 @@
 from flask import render_template, redirect, url_for, request
+from flask import jsonify
+from flask import session
+
 from . import room_bp
 from .services.date_services import today, next_day
 from ..utils.validators import Validators
 
-@room_bp.route('/', methods=['POST', 'GET'])
+@room_bp.route('/', methods=['GET'])
 def index():
+    #retrieve number of rooms
     room_types = [
             {
                 "room_typeID": 1,
                 "name": "Deluxe Room",
-                "price": 3200.00,
+                "price": 3200.50,
                 "description": "A spacious room with balcony and garden view.",
                 "note": "No smoking allowed.",
                 "size": 25.500,
@@ -31,25 +35,27 @@ def index():
                 "imageurl": "https://example.com/images/family-suite.jpg"
             },
     ]
-    if request.method == 'POST':
-        checkin = request.args.get('checkin')
-        checkout = request.args.get('checkout')
-        promocode = request.args.get('promocode')
+    
+    checkin = request.args.get('checkin')
+    checkout = request.args.get('checkout')
+    promocode = request.args.get('promocode')
 
+    #check first if the user entered a checkin and checkout date
+    if Validators.is_empty(checkin):
+        #if the user saved a checkin date
+        if 'checkin' not in session:
+
+            return render_template('rooms.html',checkin=today(), checkout=next_day(), promocode='', room_types=room_types) #CASE: initial open of a new user
+        return render_template('rooms.html',checkin=session['checkin'], checkout=session['checkout'], promocode='', room_types=room_types) #CASE: when the user exits and comebacls again
         
-        if Validators.is_empty(checkin) and Validators.is_empty(checkout):
-            return render_template('rooms.html',checkin=checkin, checkout=checkout, promocode=promocode, room_types=room_types)
-        return render_template('rooms.html',checkin=checkin, checkout=checkout, promocode=promocode, room_types=room_types)
-    else:
-        checkin = request.args.get('checkin')
-        checkout = request.args.get('checkout')
-        promocode = request.args.get('promocode')
+    #CASE: user has search room availability | save booking preference
+    session['checkin'] = checkin
+    session['checkout'] = checkout
 
-        #check first if the user entered a checkin and checkout date
-        if Validators.is_empty(checkin):
-            return render_template('rooms.html',checkin=today(), checkout=next_day(), promocode='', room_types=room_types)
-        return render_template('rooms.html', checkin=checkin, checkout=checkout, promocode=promocode, room_types=room_types)
+    return render_template('rooms.html', checkin=checkin, checkout=checkout, promocode=promocode, room_types=room_types)
+
 
 @room_bp.route('/checkout/<string:type>')
 def checkout(type):
     return render_template('checkout.html', type = type)
+
--- app/static/scripts/home.js
+++ app/static/scripts/home.js
@@ -31,28 +31,44 @@ window.addEventListener('scroll', function () {
   });
   }
 
+  //comuting total nights
+  const countNights = (checkin, checkout) => {
+    const total = document.getElementById('night-total')
+    const totalMobile = document.getElementById('night-total-mobile')
+
+    checkinSplit = checkin.split('-')
+    checkoutSplit = checkout.split('-')
+    const totalNights = parseInt(checkoutSplit[1]) - parseInt(checkinSplit[1]) 
+    total.textContent = totalNights
+    totalMobile.textContent = totalNights
+  }
+
+
   // Booking Form Submission with URL Parameters
   // let activeField = null;
   let checkInInput;
   let checkOutInput;
+  let minCheckoutDate = document.getElementById('checkin-date');
 
   // Setup Flatpickr for Check In
   flatpickr("#checkinInput", {
   dateFormat: "m-d-Y",
   onChange: function (selectedDates, dateStr) {
       if (dateStr) {
-      checkInDate = dateStr;
+      checkInDate.value = dateStr;
+      minCheckoutDate = dateStr;
       }
-      
   }
   });
 
   // Setup Flatpickr for Check Out
   flatpickr("#checkoutInput", {
   dateFormat: "m-d-Y",
+  minDate:`${minCheckoutDate.innerText}`,
   onChange: function (selectedDates, dateStr) {
       if (dateStr) {
-      checkOutDate = dateStr;
+      checkOutInput.value = dateStr;
+      
       // activeField = "checkoutInput";
       // document.getElementById("modalTime").value = "";
       // document.getElementById("timePickerModal").classList.remove("hidden");
@@ -60,27 +76,69 @@ window.addEventListener('scroll', function () {
       }
   }
   });
-  let selectedRooms = []  
 
-  const totalNight =  document.getElementById('room-total')
-  totalNight.textContent = 0
-  let room1 = 0
-  let room2 = 0
-  let room3 = 0
+  //get the price elemetn
+  const priceElem = document.getElementById('total-price')
+  const priceElemMobile = document.getElementById('total-price-mobile')
+  let totalPrice = 0
+  const computeRoomPrice = (basePrice, qty) => {
+    const total = basePrice * qty
+    totalPrice += total
+  }
+  
+  //Room Tracking Code
+  const displayRooms = document.getElementById('display-rooms')
+  let room = {}
 
- const roomDropDown = document.getElementById('select-room-dropdown') 
- roomDropDown.addEventListener('change', function() {
-  room1 = this.value
-  count = parseInt(room1) + parseInt(room2);
-  totalNight.textContent = count
- })
+  //Add event listeners to all dropdowns
+  const roomDropDowns = document.querySelectorAll('#select-room-dropdown')
+  const roomTypeNames = document.querySelectorAll('#room-name') //get all of the room name
+  const roomPrices = document.querySelectorAll('#room-price')
+  
+  let count = 0 //for accessing the room type nodelist. SEE: roomTypeIds 
+  roomDropDowns.forEach(dropdown => {
+      const roomName =  roomTypeNames[count].outerText //retrieve the text value from the nodelist
+      const roomPrice = roomPrices[count].outerText
+      
+      dropdown.addEventListener('change', function() {     
+          room[roomName] = {num: this.value, price:roomPrice} //Data selected by the customer. Actual data structure     
 
- const roomDropDown2= document.getElementById('select-room-dropdown2') 
- roomDropDown2.addEventListener('change', function() {
-  room2 = this.value
-  count = parseInt(room1) + parseInt(room2);
-  totalNight.textContent = count
- })
+          
+
+          //CASE: Deleted the old content
+            if (displayRooms.innerText !== '') {
+              displayRooms.innerHTML = "";
+            }
+            
+          for (key in room) {
+            if(room[key].num === '0') {
+              delete room[key]
+              console.log('deleted')
+            }
+            
+            //for display
+            //assign new value
+            const newPara = document.createElement('span')
+            newPara.classList = "text-xs"
+            //Outputs the newest data
+            newPara.innerText = `${room[key].num}x ${key}`
+            displayRooms.appendChild(newPara)
+          }
+          //Compute first and update the total
+          computeRoomPrice(roomPrice, this.value)
+          priceElem.innerText = parseFloat(totalPrice)
+          priceElemMobile.innerText = parseFloat(totalPrice)
+          console.log(room)
+      })
+
+      count += 1
+  })
+  
+  
+
+
+
+
 
 
   // Confirm time selection
@@ -99,10 +157,6 @@ window.addEventListener('scroll', function () {
 //   document.getElementById("modalCancel").addEventListener("click", () => {
 //   document.getElementById("timePickerModal").classList.add("hidden");
 //   });
-
-
-
-
 
 
   // for the room page
